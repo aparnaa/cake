793d192fcdac78038b46704bc6b896ac8a61c3d0
aparnaa <aparnaa.m27@gmail.com>
2012-06-03 19:30:24 +0530

    smc10

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,8 +2,8 @@ class Notifier < ActionMailer::Base
   default :from => "admin@gmail.com"
   def send_mail(mail,myevent)
   @title=myevent.title
-  @event_date=myevent.date
-  @event_time=myevent.time.strftime("%H:%M")
+  @date=myevent.date
+  @time=myevent.time.strftime("%H:%M")
   @venue=myevent.venue
   @description=myevent.description
   @mail=mail 
