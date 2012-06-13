6a170440477a8b172fba24a2212f767356c7d855
aparnaa <aparnaa.m27@gmail.com>
2012-06-13 15:20:03 +0530

    smc3

--- db/migrate/20120529113544_add_body_to_invitation.rb
+++
@@ -1,9 +0,0 @@
-class AddBodyToInvitation < ActiveRecord::Migration
-  def self.up
-    add_column :invitations, :body, :text
-  end
-
-  def self.down
-    remove_column :invitations, :body
-  end
-end
--- db/migrate/20120529114709_remove_body_to_invitation.rb
+++
@@ -1,9 +0,0 @@
-class RemoveBodyToInvitation < ActiveRecord::Migration
-  def self.up
-    remove_column :invitations, :body
-  end
-
-  def self.down
-    add_column :invitations, :body, :text
-  end
-end
